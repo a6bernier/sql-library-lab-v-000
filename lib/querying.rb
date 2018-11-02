77278fd346536b890cc483b0ddedc98706a7821c
Andrew Bernier <a6bernier@gmail.com>
2018-11-02 22:31:08 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,7 +21,7 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name
   FROM authors
-  INNER JOIN subgenres
+  INNER JOIN subgenres.name
   ON authors.name = subgenres.name;
   "
 end
