67dcc1bd2bef8055d72af71f5c8732e79b5202b7
Andrew Bernier <a6bernier@gmail.com>
2018-11-02 22:19:17 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT COUNT species FROM characters WHERE species = 'human'"
+  "SELECT species COUNT(*) FROM characters WHERE species = 'human'"
 end
 
 def select_name_and_series_subgenres_of_authors
